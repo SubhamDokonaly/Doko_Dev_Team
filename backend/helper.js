498e79fa7a48363d006f521fb38b1208f2ccee6c
SubhamDokonaly <subham.sidhartha@dokonaly.com>
2023-02-16 10:58:49 +0530

    added 3 function

--- backend/helper.js
+++ backend/helper.js
@@ -1,1 +1,13 @@
 /**expect mongose function other function added here */
+
+
+function removeSpaces(x){
+    return x.split(" ").filter((y)=> y).join(" ");
+}
+const isValidRequestBody = function (requestBody) {
+    return Object.keys(requestBody).length > 0;
+}
+
+const isValidObjectId = function (objectId) {
+    return mongoose.Types.ObjectId.isValid(objectId)
+}
