beb7134533994aafe08868663cfb3916f802b5cf
uvandoko <uvansankar@dokonaly.com>
2023-02-16 16:32:58 +0530

    helper

--- backend/helper.js
+++ backend/helper.js
@@ -1,13 +1,14 @@
 /**expect mongose function other function added here */
 
 
-function removeSpaces(x){
-    return x.split(" ").filter((y)=> y).join(" ");
-}
 const isValidRequestBody = function (requestBody) {
     return Object.keys(requestBody).length > 0;
 }
 
 const isValidObjectId = function (objectId) {
     return mongoose.Types.ObjectId.isValid(objectId)
+}
+
+function returnFunction (statusData, responseMsg, responseData) {
+    return res.send({status: statusData, response: responseMsg, data: responseData})
 }
--- frontend/helper.js
+++ frontend/helper.js
@@ -1,1 +1,10 @@
 /**Add your frontend common function here */
+
+const idMatchLoop = (data, id, method) => {
+    for (let i = 0; i < data.length; i++) {
+        if (data[i][method] === id) { 
+          return [data[i]]
+        }
+    }
+    return ""
+}
