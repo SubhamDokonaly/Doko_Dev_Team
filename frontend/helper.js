8579c1d1fe1102ba9502c83f54b4bc76d0b879d6
yogeshdoko <yogesh.s@dokonaly.com>
2023-02-16 18:30:32 +0530

    Add fetchData function

--- frontend/helper.js
+++ frontend/helper.js
@@ -1,1 +1,30 @@
 /**Add your frontend common function here */
+
+/**  fetchFunction for fetching Data */
+async function fetchData(
+  { url, method = "GET", Authorization, Type } = {},
+  data
+) {
+  let fetchObject = {
+    method: method,
+    headers: {
+      Authorization: Authorization,
+      Type: Type,
+      "Content-Type": "application/json",
+    },
+  };
+  if (method === "POST") {
+    fetchObject.headers.body = JSON.stringify(data);
+  }
+  try {
+    let response = await fetch(url, fetchObject);
+    let responseData = await response.json();
+    if (responseData.status === 1 || responseData.status === true) {
+      return { result: true, data: JSON.parse(responseData.response) };
+    } else {
+      return { result: false, data: responseData.response };
+    }
+  } catch (error) {
+    return { result: false, data: error };
+  }
+}
