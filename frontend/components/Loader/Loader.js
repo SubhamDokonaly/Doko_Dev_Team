f9b41ff110fb9d573457cd1b2cd0423c8c1df608
balaji.r@dokonaly.com <balaji.r@dokonaly.com>
2023-02-16 17:35:09 +0530

    create checkduplicate and viewfile function and create loader component

---
+++ frontend/components/Loader/Loader.js
@@ -0,0 +1,13 @@
+import "./Loader.css"
+import Dokologo from './dokologo.png';
+
+const SpinnerLoader = () => {
+  return (
+    <div>
+      <div className="loader-container">
+        <img src={Dokologo} className="rotate" alt="Loader"/>
+      </div>
+    </div>
+  )
+}
+export default SpinnerLoader;
--- frontend/helper.js
+++ frontend/helper.js
@@ -1,1 +1,15 @@
 /**Add your frontend common function here */
+
+export function CheckDuplicates(array) {
+    const removeDuplicates = [...new Set(array.map((e) => JSON.stringify(e)))].map((e) => JSON.parse(e))
+    return removeDuplicates
+};
+
+export function viewFile(file) {
+    let win = window.open();
+    win.document.write(
+        '<iframe src="' +
+        file +
+        '" frameborder="0" style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;" allowfullscreen></iframe>'
+    )
+};
