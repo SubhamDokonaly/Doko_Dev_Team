8cbf2c2d788b5312437d0ff156a44462d6930c3e
balaji.r@dokonaly.com <balaji.r@dokonaly.com>
2023-02-20 15:53:05 +0530

    helper functions

--- frontend/helper.js
+++ frontend/helper.js
@@ -1,51 +1,57 @@
 /**Add your frontend common function here */
-
-export function CheckDuplicates(array) {
-    const removeDuplicates = [...new Set(array.map((e) => JSON.stringify(e)))].map((e) => JSON.parse(e))
-    return removeDuplicates
-};
-
-export function viewFile(file) {
-    let win = window.open();
-    win.document.write(
-        '<iframe src="' +
-        file +
-        '" frameborder="0" style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;" allowfullscreen></iframe>'
-    )
-};
-
-//ID Match Loop - idMatchLoop(originData.origin, '_id', e.pol)
-const idMatchLoop = (data, key, value) => {
-    for (let i = 0; i < data.length; i++) {
-        if (data[i][key] === value) { 
-          return [data[i]]
-        }
-    }
-    return ""
+// ---------- Check Duplicates Function ------------ //
+const CheckDuplicates = (array) => {
+  if (typeof array !== 'object') {
+    return 'Should be an Array or Object'
   }
+  const removeDuplicates = [...new Set(array.map((e) => JSON.stringify(e)))].map((e) => JSON.parse(e))
+  return removeDuplicates
+};
+// ---------- Open File New Window Function ------------ //
+const OpenFileNewWindow = (fileData) => {
+  if (typeof fileData !== 'string') {
+    return 'Uploaded File is Not a String';
+  };
+  if (!fileData.includes('base64')) {
+    return 'Uploaded File is Not Base64 file';
+  };
+  let win = window.open();
+  win.document.write(
+    '<iframe src="' +
+    fileData +
+    '" frameborder="0" style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;" allowfullscreen></iframe>'
+  );
+};
 
+const idMatchLoop = (data, key, value) => {
+  for (let i = 0; i < data.length; i++) {
+    if (data[i][key] === value) {
+      return [data[i]]
+    }
+  }
+  return ""
+}
 
+// idMatchLoop(originData.origin, '_id', e.pol)
 
 /**      File Reader Function         */
-const fileReaderFunction = (event,fileType,fileSize,errorMessage) =>{
+export const fileReaderFunction = (file, fileType, fileSize) => {
   let fileDatas = {}
-  let file = event.target.files[0];
-  let reader = new FileReader ();
+  let file = file.target.files[0];
+  let reader = new FileReader();
   reader.readAsArrayBuffer(file)
-  if (event.target.files.length > 0) {
+  if (e.target.files.length > 0) {
     return new Promise((resolve, reject) => {
-      reader.onload = (event) => {
+      reader.onload = (e) => {
         if (!file) {
-          reject(errorMessage.NoFileError); 
+          reject("Upload file first");
         } else if (!fileType.includes(file.type)) {
-          reject(errorMessage.fileTypeErr);
+          reject(`Upload only ${fileType}`);
         } else if (file.size > fileSize) {
-          reject(errorMessage.fileSizeErr);
+          reject(`Upload file under ${fileSize} MB`);
         }
-        fileDatas.fileData = event.target.result
+        fileDatas.fileData = e.target.result
         fileDatas.fileName = file.name
-        fileDatas.fileSize = file.size
-        fileDatas.fileType = file.type
         resolve(fileDatas)
       }
     })
@@ -53,20 +59,15 @@ const fileReaderFunction = (event,fileType,fileSize,errorMessage) =>{
 }
 
 /**      File Reader Function Calling format and handling the output      */
-
-// const handleFile = (e) => {
-//   let errorMessage={
-//     NoFileError:`Upload file first`,
-//     fileTypeErr:`Upload only Pdf`,
-//     fileSizeErr:`Upload file under 3 MB`,
-//   }
-//   fileReaderFunction(e,fileType,fileSize,errorMessage).then((data) => { 
-//       Get the resolved output
-//   }).catch((error) => { 
-//        Get the rejected ouput
-//   });
-// };
-
+//   const handleFile =  (e) => {
+//     fileReaderFunction(e,fileType,fileSize)
+//     .then((data) => { 
+//         Get the resolved output
+//     })
+//     .catch((error) => { 
+//         Get the rejected ouput
+//     }); 
+//   };
 
 /**  fetchFunction for fetching Data */
 async function fetchData(
