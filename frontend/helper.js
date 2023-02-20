0bdad06894a27b69cbc81fea1997508a94263f54
Vijayaragavan-1999 <vijayaragavan.s@dokonaly.com>
2023-02-20 10:34:59 +0530

    Getting the error message as an parameter

--- frontend/helper.js
+++ frontend/helper.js
@@ -27,39 +27,46 @@ const idMatchLoop = (data, key, value) => {
 
 
 /**      File Reader Function         */
-export const fileReaderFunction = (file,fileType,fileSize) =>{
-    let fileDatas = {}
-    let file = file.target.files[0];
-    let reader = new FileReader ();
-    reader.readAsArrayBuffer(file)
-    if (e.target.files.length > 0) {
-      return new Promise((resolve, reject) => {
-        reader.onload = (e) => {
-          if (!file) {
-            reject("Upload file first"); 
-          } else if (!fileType.includes(file.type)) {
-            reject(`Upload only ${fileType}`);
-          } else if (file.size > fileSize) {
-            reject(`Upload file under ${fileSize} MB`);
-          }
-          fileDatas.fileData = e.target.result
-          fileDatas.fileName = file.name
-          resolve(fileDatas)
+const fileReaderFunction = (event,fileType,fileSize,errorMessage) =>{
+  let fileDatas = {}
+  let file = event.target.files[0];
+  let reader = new FileReader ();
+  reader.readAsArrayBuffer(file)
+  if (event.target.files.length > 0) {
+    return new Promise((resolve, reject) => {
+      reader.onload = (event) => {
+        if (!file) {
+          reject(errorMessage.NoFileError); 
+        } else if (!fileType.includes(file.type)) {
+          reject(errorMessage.fileTypeErr);
+        } else if (file.size > fileSize) {
+          reject(errorMessage.fileSizeErr);
         }
-      })
-    }
+        fileDatas.fileData = event.target.result
+        fileDatas.fileName = file.name
+        fileDatas.fileSize = file.size
+        fileDatas.fileType = file.type
+        resolve(fileDatas)
+      }
+    })
   }
+}
 
 /**      File Reader Function Calling format and handling the output      */
-//   const handleFile =  (e) => {
-//     fileReaderFunction(e,fileType,fileSize)
-//     .then((data) => { 
-//         Get the resolved output
-//     })
-//     .catch((error) => { 
-//         Get the rejected ouput
-//     }); 
-//   };
+
+// const handleFile = (e) => {
+//   let errorMessage={
+//     NoFileError:`Upload file first`,
+//     fileTypeErr:`Upload only Pdf`,
+//     fileSizeErr:`Upload file under 3 MB`,
+//   }
+//   fileReaderFunction(e,fileType,fileSize,errorMessage).then((data) => { 
+//       Get the resolved output
+//   }).catch((error) => { 
+//        Get the rejected ouput
+//   });
+// };
+
 
 /**  fetchFunction for fetching Data */
 async function fetchData(
